baca013274f7921ff3cddfd5e43527201c51e442
Kiwi <Kiwi@123>
2025-08-21 11:07:18 +0530

    week3 assignment folder added

---
+++ assignment/git-assignment-142502015/README.md
@@ -0,0 +1,2 @@
+Roll Number: 142502015
+Timestamp: 20/8/2025 11:33:41
---
+++ assignment/git-assignment-142502015/src/calculator.py
@@ -0,0 +1,8 @@
+a = int(input('first Number:'))
+b = int(input('Second Number:'))
+
+def add(a,b):
+	return a+b
+
+print('Summation is:',add(a,b))
+
